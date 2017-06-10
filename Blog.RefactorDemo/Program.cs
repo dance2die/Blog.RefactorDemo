522dccca8d8a09dfbfdc28aca86c66488da6b476
dance2die <dontbothermewithspam@gmail.com>
2017-06-10 14:54:43 -0400

    initial import

--- Blog.RefactorDemo/Program.cs
+++ Blog.RefactorDemo/Program.cs
@@ -1,15 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Blog.RefactorDemo
 {
-	class Program
+	public class Program
 	{
-		static void Main(string[] args)
+		public static void Main(string[] args)
 		{
+			string text = "This is a long text to be searched";
+			// A word to search within "text".
+			string searchWord = "long";
+
+			int lastIndex = GetIndexAfterFoundWord(text, searchWord);
+			Console.WriteLine(lastIndex);
+		}
+
+		/// <summary>
+		/// Find last index of found word within "text"
+		/// </summary>
+		/// <returns>
+		/// text = "Hello, World!""
+		/// searchWord = "World"
+		/// then the result is 12 (right before "!")
+		/// </returns>
+		/// <remarks>Search "searchWord" within "text" using KMP (Knuth-Morris-Pratt) Algorithm.</remarks>
+		private static int GetIndexAfterFoundWord(string text, string searchWord)
+		{
+			// Build Prefix KMP Table
+
+			// Search searchWord using the table.
+			
+			// return the index after found word
 		}
 	}
 }
