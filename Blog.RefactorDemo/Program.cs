b8eabd4bc353bf0c6ab00615fd02f54855618c0a
dance2die <dontbothermewithspam@gmail.com>
2017-06-11 08:56:44 -0400

    implemeneted "IndexIterator"

--- Blog.RefactorDemo/Program.cs
+++ Blog.RefactorDemo/Program.cs
@@ -4,6 +4,22 @@ using System.Linq;
 
 namespace Blog.RefactorDemo
 {
+	public class IndexIterator
+	{
+		private readonly ITextSearch _textSearch;
+
+		public IndexIterator(ITextSearch textSearch)
+		{
+			_textSearch = textSearch;
+		}
+
+		public int GetIndexAfterFoundWord4(string text, string searchWord)
+		{
+			int[] found = _textSearch.Find(text, searchWord);
+			return found.First() + searchWord.Length;
+		}
+	}
+
 	public class Program
 	{
 		public static void Main(string[] args)
